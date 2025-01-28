682d5518ea33749600f086f521d5b5e60ef48c83
Dorian <ddeneuchatel@nids.fr>
2025-01-28 17:22:35 +0100

    all

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// main.js
+const burgerMenu = document.getElementById("burger-menu");
+const navbar = document.getElementById("navbar");
+
+burgerMenu.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
