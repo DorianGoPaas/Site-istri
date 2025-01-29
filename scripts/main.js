21e12ddfcee0223b901f3f0b665d116af308d238
Dorian <ddeneuchatel@nids.fr>
2025-01-29 12:10:57 +0100

    Défilement des marques

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,9 @@ const navbar = document.getElementById("navbar");
 burgerMenu.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollContainer = document.querySelector(".partners-scroll");
+    const clones = scrollContainer.innerHTML;
+    scrollContainer.innerHTML += clones; 
+});
